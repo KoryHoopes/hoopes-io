9e53e8d1626d63fd1b51780c6f37919cd7001260
Kory Hoopes <kory@hey.com>
2020-12-28 20:15:09 -0700

    Adding home component
    Adding the home component where the main site data will live.

--- www-hoopes-io/src/app/app.module.ts
+++ www-hoopes-io/src/app/app.module.ts
@@ -1,35 +1,36 @@
 import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+// Flex-Layout
 import { FlexLayoutModule } from '@angular/flex-layout';
+// Material
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// Font Awesome
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { AppComponent } from './app.component';
-import { NavigationComponent } from './navigation/navigation.component';
-
-
-
-
-
+// App Components
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, NavigationComponent],
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    HomeComponent
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-
     // Flex-Layout
     FlexLayoutModule,
-
     // Material
     MatButtonModule,
     MatDialogModule,
     MatIconModule,
     MatToolbarModule,
-
     // Font Awesome
     FontAwesomeModule
   ],
