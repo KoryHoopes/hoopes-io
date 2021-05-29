196f934f16d4ccd61380d341e55f15ed2ffb605f
Kory Hoopes <kory@hey.com>
2021-05-28 21:44:04 -0600

    Mobile menu fixes and adding more social icons
    Fixed an issue with the spacing of mobile menu social icons as well as added an icon and link to my Replit.com profile.

--- www-hoopes-io/src/app/components/header/header.data.ts
+++ www-hoopes-io/src/app/components/header/header.data.ts
@@ -2,6 +2,7 @@
 import { trigger, transition, style, animate } from '@angular/animations';
 // Font Awesome
 import { faBars } from '@fortawesome/free-solid-svg-icons/faBars';
+import { faCode } from '@fortawesome/free-solid-svg-icons/faCode';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope';
 import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
@@ -35,16 +36,18 @@ const animations = [
 ];
 const icons = {
   bars: faBars,
+  email: faEnvelope,
   gitHub: faGithub,
   linkedIn: faLinkedinIn,
+  replit: faCode,
   times: faTimes
 };
 const site = {
   title: 'hoopes.io',
-  email: faEnvelope,
+  email: icons.email,
   baseUrl: '/',
   mobileMenu: {
-    icon: faBars,
+    icon: icons.bars,
     show: false
   }
 };
@@ -58,6 +61,11 @@ const socialMedia = [
     name: 'LinkedIn',
     icon: icons.linkedIn,
     url: 'https://www.linkedin.com/in/kory-hoopes'
+  },
+  {
+    name: 'Replit.com',
+    icon: icons.replit,
+    url: 'https://replit.com/@KoryHoopes'
   }
 ];
 
